67e48e3db4ef71685fd540bad5977ca1ceb7917e
RodrigoFonsecaG <rodrigofon1@hotmail.com>
2021-11-08 13:22:47 -0300

    teste2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const server = express();
 
 const nunjucks = require('nunjucks');
-nunjucks.configure("./", {
+nunjucks.configure("src", {
     express: server,
     noCache: true
 })
