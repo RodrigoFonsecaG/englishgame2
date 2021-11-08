84aabec343f0105e720ea882ab685743e7b62452
RodrigoFonsecaG <rodrigofon1@hotmail.com>
2021-11-08 15:36:51 -0300

    final

--- public/modal.js
+++ public/modal.js
@@ -1,17 +1,12 @@
 function scheduleModal() {
   const scheduleButton = document.querySelector('.add-word');
   const modalContainer = document.querySelector('.modal-container');
-  const closeButton = document.querySelector('.close');
   const modal = document.querySelector('.modal');
 
   scheduleButton.addEventListener('click', (event) => {
     event.preventDefault();
     modalContainer.classList.add('ativo');
   });
-
-  closeButton.addEventListener('click', () => {
-      modalContainer.classList.remove('ativo');
-  })
 
   modalContainer.addEventListener('click', (event) => {
     (modal.contains(event.target)) ? '' : modalContainer.classList.remove('ativo');
